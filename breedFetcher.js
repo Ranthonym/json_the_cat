546c5e28f185e41317e034ec0837fdd33643d534
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2020-02-13 21:59:15 +0000

    added breedFetcher code and mocha testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,11 +1,17 @@
 const request = require('request');
-const url = 'https://api.thecatapi.com/v1/breeds/search?q=';
-const inputBreed = process.argv[2];
-request (url + inputBreed, (error, response) => { 
-  const data = JSON.parse(response.body);
-  if (data.length > 0) {
-    console.log(data[0].description);
-  } else {
-    console.log("Breed not found");
-  }
-});
+const fetchBreedDescription = (breedName, callback) => {
+  const url = 'https://api.thecatapi.com/v1/breeds/search?q=';
+  request (url + breedName, (error, response) => {
+    if (error) {
+      callback(error, null);
+      return;
+    } else  { 
+        const data = JSON.parse(response.body);
+        if (data[0])
+        callback(null, data[0].description.trim());
+        else callback(null, 'No breed exists');
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
